b5328bbf904f12ed5e551380e37c394d98a4fa85
Kurbsi <khirschmueller@me.com>
2020-01-03 15:13:21 +0100

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -7,7 +7,6 @@ alias lk='ls -lF'
 alias serial0='screen /dev/ttyUSB0 115200'
 alias serial1='screen /dev/ttyUSB1 115200'
 alias weather='curl wttr.in/munich'
-alias z='sudo mount -t cifs -o user=hirschmuellerk //10.210.64.209/VISPIRON /mnt/z && cd /mnt/z'
 alias lo='libreoffice'
 alias pi="ssh pi@192.168.178.22"
 
