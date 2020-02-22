fe6ca79aebbd76aee4c3df03643d1c5fade866b6
Korbinian Hirschmueller <khirschmueller@me.com>
2020-02-22 10:23:00 +0100

    added lock script

---
+++ bin/lock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+IMG=/tmp/screen.png
+scrot $IMG
+convert $IMG -scale 10% -scale 1000% $IMG
+[[ -f $1 ]] && convert $IMG $1 -gravity center -composite -matte $IMG
+# dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org.mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+i3lock -u -i /tmp/screen.png
+rm /tmp/screen.png
+
