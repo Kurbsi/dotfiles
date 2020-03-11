a7f5c239996af243830eba7e21b13af8fecec2ea
Korbinian Hirschmueller <korbinian.hirschmueller@partner.bmw.de>
2020-03-11 10:34:07 +0100

    make image lock screen more blocky

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 IMG=/tmp/screen.png
 scrot $IMG
-convert $IMG -scale 10% -scale 1000% $IMG
+convert $IMG -scale 5% -scale 2000% $IMG
 [[ -f $1 ]] && convert $IMG $1 -gravity center -composite -matte $IMG
-# dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org.mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 i3lock -f -i /tmp/screen.png
 rm /tmp/screen.png
 
