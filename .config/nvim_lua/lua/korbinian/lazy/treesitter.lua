df2f72a133d9aa0c8552e51ca9d6cfa6d67704be
Pia Hirschmüller <piahirschmuller@MacBook-Air-von-Pia.local>
2026-03-29 00:03:04 +0100

    Update treesitter

--- .config/nvim_lua/lua/korbinian/lazy/treesitter.lua
+++ .config/nvim_lua/lua/korbinian/lazy/treesitter.lua
@@ -1,15 +1,5 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ":TSUpdate",
-    config = function()
-        require('nvim-treesitter.configs').setup({
-            modules = {},
-            sync_install = false,
-            ensure_installed = { "bash", "c", "cmake", "cpp", "gitignore", "gitcommit", "go", "gomod", "json", "lua", "markdown", "rust", "vimdoc" },
-            auto_install = false,
-            ignore_install = {},
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
 }
